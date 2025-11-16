48c3caa6aaaeb4b39e367e24b4f17ccd20e9d1be
Lennard Beers <l.beers@outlook.de>
2025-11-16 05:20:31 +0100

    fix: unlink openapi.json

--- scripts/generate-client.js
+++ scripts/generate-client.js
@@ -1,6 +1,6 @@
 import { exec } from 'child_process';
 import { existsSync } from 'fs';
-import { mkdir, writeFile, unlink } from 'fs/promises';
+import { mkdir, unlink, writeFile } from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { promisify } from 'util';
